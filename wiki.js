54d11924d6f4e710be915054b365c799d3301818
Andrej T <andrejtrajchevski@gmail.com>
2012-11-19 15:17:11 +0100

    Improved regex filtering and cache.

--- wiki.js
+++ wiki.js
@@ -1,17 +1,36 @@
+/* A Chrome extension that adds a one-sentence description
+to Wikipedia links.
+
+This is how it works: when you mouse-over a link in a Wiki
+article, it fetches the first sentence of the text and
+adds it to the "title" attribute of the link.
+*/
+
+
 $(function(){
+
+    var cache = {};
+
+    var cacheget = function(href, cb) {
+        if (cache[href]) cb(cache[href]);
+        else $.get(href, function(data) {
+            cache[href] = data;
+            cb(data);
+        });
+    }
 
 	$("div#mw-content-text a").mouseover(function(e){
 		
-		$.get(this.href, function(data){	
-			// Get the first paragraph as plain text
-			var first_paragraph = $(data).find('div#mw-content-text p')[0]
+		cacheget(this.href, function(data){	
+			// Get the first paragraph as plain text without citations.
+			var first_paragraph = $(data).find('div#mw-content-text > p').eq(0)
+                .text().replace(/\[[1-9]*\]/, '');
 			
 			// Extract the first sentence
-			// TODO: regex based, not to split on inner dots
-			var first_sentence = $(first_paragraph).text().split('.')[0] + '.'
-			var first_sentence = first_sentence.replace(/\[[1-9]*\]/, '');
+            var sentence_end = first_paragraph.match(/["'\)0-9a-zA-Z]{2}\.(\s|$)/);
+            var first_sentence = first_paragraph.substr(0, sentence_end.index + sentence_end[0].length);
 			
 			e.currentTarget.title = first_sentence;
 		})
 	});
-})
+});
